8dae6ab0a1b5ddd2a83dc50cf451013cc4eb8543
Jatin Mayekar <jama3012@colorado.edu>
2023-09-20 18:00:01 -0400

    add two text fields

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import { Page, Text, Image, Display, Button, Grid } from '@geist-ui/core'
+import { Page, Text, Image, Display, Input, Spacer } from '@geist-ui/core'
+import React, { useState } from 'react';
 
-const gh = 'https://github.com/geist-org/react'
-const docs = 'https://react.geist-ui.dev'
+//const gh = 'https://github.com/geist-org/react'
+//const docs = 'https://react.geist-ui.dev'
 
 const App = () => {
-  const redirect = url => window.open(url)
-
+  //const redirect = url => window.open(url)
+  const [inputValue, setInputValue] = useState('');  // State to store the input value
+  
   return (
     <Page dotBackdrop width="800px" padding={0}>
       <Display
-        title="Geist UI"
+        title="Nerd AI"
         caption={
           <>
             Welcome to{' '}
             <Text span b>
-              Geist UI
+              Nerd AI
             </Text>{' '}
             and start learning more !
-          </>
+            <Spacer h={.5} />
+            <Input clearable='true' placeholder="Ask away..." width="100%"
+            onChange={(e) => setInputValue(e.target.value)}  // Update state on input change
+            />
+            <br /><br />
+            <Input 
+              type="success"
+              readOnly  // Make the second input read-only
+              value={inputValue}  // Display the stored value
+              width="100%" 
+            />
+        </>
         }>
-        <Image src="/geist-banner.png" draggable={false} />
+        <Image width="280px" height="160px" src="/geist-banner.png" draggable={false} />
       </Display>
-      <Grid.Container justify="center" gap={3} mt="100px">
-        <Grid xs={20} sm={7} justify="center">
-          <Button shadow type="secondary-light" onClick={() => redirect(gh)}>
-            GitHub Repo
-          </Button>
-        </Grid>
-        <Grid xs={0} sm={3} />
-        <Grid xs={20} sm={7} justify="center">
-          <Button onClick={() => redirect(docs)}>Documentation Site</Button>
-        </Grid>
-      </Grid.Container>
     </Page>
   )
 }
