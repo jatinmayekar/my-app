b5105b91ded4760a9c4e6cda8ab282da0209f686
Jatin Mayekar <jama3012@colorado.edu>
2023-09-29 21:44:51 -0400

    add audio output and placeholder audio input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import { Page, Text, Image, Display, Input, Button, Spacer } from '@geist-ui/core'
-import React, { useState } from 'react';
+import { Page, Text, Image, Display, Input, Button, Spacer, Textarea } from '@geist-ui/core'
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [userInput, setUserInput] = useState('');
   const [apiResponse, setApiResponse] = useState('');
+  const [isRecording, setIsRecording] = useState(false);
+
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
 
   const handleSendToAPI = async () => {
     try {
       const result = await axios.post('http://localhost:5000/get-response', { userInput: userInput });
+      const responseText = result.data.response;
       setApiResponse(result.data.response);
+
+      // After displaying the text, get the audio and play it
+      const audioResult = await axios.post('http://localhost:5000/get-response-audio', { text: responseText });
+      const audioBlob = new Blob([audioResult.data], { type: 'audio/wav' });
+      const audioURL = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioURL);
+      audio.play();
+
     } catch (error) {
       console.error("Error calling the backend:", error);
+    }
+  };
+
+  const handleRecordAudio = async () => {
+    if (!isRecording) {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorderRef.current = new MediaRecorder(mediaStream);
+      mediaRecorderRef.current.ondataavailable = event => {
+        audioChunksRef.current.push(event.data);
+      };
+      mediaRecorderRef.current.onstop = async () => {
+        const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/mp3' });
+        const audioURL = URL.createObjectURL(audioBlob);
+        const downloadLink = document.createElement('a');
+        downloadLink.href = audioURL;
+        downloadLink.download = 'recorded_audio.mp3';
+        downloadLink.click();
+      };
+      mediaRecorderRef.current.start();
+      setIsRecording(true);
+    } else {
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
     }
   };
 
@@ -35,12 +71,16 @@ const App = () => {
               width="100%" 
             />
             <br /><br />
+            <Button loading={false} onClick={handleRecordAudio}>
+              {isRecording ? "Stop Recording" : "Start Recording"}
+            </Button>
             <Button onClick={handleSendToAPI}>Send to ChatGPT</Button>
             <br /><br />
-            <Input 
+            <Textarea
+              width="100%"
+              resize='both'
               value={apiResponse}
               readOnly
-              width="100%" 
             />
           </>
         }>
@@ -50,4 +90,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
